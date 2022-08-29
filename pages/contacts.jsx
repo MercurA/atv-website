78fd9d3a5f73561cfb049d43b284e2eb23bc6191
MercurA <constandacheh@gmail.com>
2022-08-29 16:07:13 +0300

    WIP map

--- pages/contacts.jsx
+++ pages/contacts.jsx
@@ -1,12 +1,19 @@
 import styles from './styles.module.scss'
 import Input from '../components/inputs/Input'
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import strings_RO from '../constants/strings'
 import Textarea from '../components/inputs/Textarea'
-// import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'
+import { MapContainer } from 'react-leaflet'
 
 const Contacts = () => {
     const [formState, setFormState] = useState({})
+    const [W, setW] = useState({})
+
+    useEffect(() => {
+        if(typeof window !== 'undefined') {
+            window && setW(window)
+        }
+    },[])
 
     const handleChange = (e) => {
         setFormState({
@@ -14,19 +21,29 @@ const Contacts = () => {
             [e.target.id]: e.target.value
         })
     }
+
+    const renderMap = () => {
+        if(W) {
+            return (
+                 <MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false}>
+                    <TileLayer
+                        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                    />
+                    <Marker position={[51.505, -0.09]}>
+                        <Popup>
+                            A pretty CSS3 popup. <br /> Easily customizable.
+                        </Popup>
+                    </Marker>
+                </MapContainer>
+            )
+        }
+    }
+
     return (
         <div className={styles.container}>
-            {/* {window && (<MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false}>
-                <TileLayer
-                    attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-                    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-                />
-                <Marker position={[51.505, -0.09]}>
-                    <Popup>
-                        A pretty CSS3 popup. <br /> Easily customizable.
-                    </Popup>
-                </Marker>
-            </MapContainer>)} */}
+            
+            {renderMap()}
             <Input name={strings_RO.input.name} id={'name'} placeholder={'Ex. Andrei Popescu'} handleOnChange={handleChange} />
             <Input name={strings_RO.input.email} id={'email'} placeholder={'Ex. exemplu@gmail.com'} handleOnChange={handleChange} />
             <Input name={strings_RO.input.noTelephone} id={'telephone'} placeholder={'Ex. 004074123123'} handleOnChange={handleChange} />
