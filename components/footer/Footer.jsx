9a64f13967af4398dcd86f85c74e8592e8867e8a
MercurA <constandacheh@gmail.com>
2022-09-01 00:37:07 +0300

    added mobile footer

--- components/footer/Footer.jsx
+++ components/footer/Footer.jsx
@@ -1,15 +1,47 @@
-import styles from './styles.module.scss'
 import Link from 'next/link'
 
+import styles from './styles.module.scss'
+import Device from "../../utils/index";
+
 const Footer = () => {
-    return (
-        <div className={styles.container}>
-            <div className={styles.terms}>
-                <Link href={'/conditios'}><a target={"_blank"}>{'Termeni si Conditii'}</a></Link>
-                <Link href={'/GDPR'}><a target={"_blank"}>{'Politica de confidentialitate'}</a></Link>
+
+    const renderDesktopFooter = () => {
+        return (
+            <div className={styles.container}>
+                <div className={styles.terms}>
+                    <Link href={'/conditios'}><a target={"_blank"}>{'Termeni si Conditii'}</a></Link>
+                    <Link href={'/GDPR'}><a target={"_blank"}>{'Politica de confidentialitate'}</a></Link>
+                </div>
+                <div className={styles.signature}>{'Made with'}&#129505;{'by Cypher'}</div>
             </div>
-            <div className={styles.signature}>{'Made with'}&#129505;{'by Cypher'}</div>
-        </div>
+        )
+    }
+
+    const renderMobileFooter = () => {
+        return (
+            <div className={styles.mobileFooter}>
+                <div className={styles.terms}>
+                    <Link href={'/conditios'}><a target={"_blank"}>{'Termeni si Conditii'}</a></Link>
+                    <Link href={'/GDPR'}><a target={"_blank"}>{'Politica de confidentialitate'}</a></Link>
+                </div>
+                <div className={styles.signature}>{'Made with'}&#129505;{'by Cypher'}</div>
+            </div>
+        )
+    }
+
+    const renderMenuType = (isMobile) => {
+        if(isMobile) {
+            return renderMobileFooter()
+        } else {
+            return renderDesktopFooter()
+        }
+    }
+
+    return (
+        <Device>
+            {(isMobile) => renderMenuType(isMobile)}
+        </Device>
+
     )
 }
 
