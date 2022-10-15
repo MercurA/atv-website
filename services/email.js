2c6534b821ce7d04698109ac34e18d5ca77cd7da
MercurA <constandacheh@gmail.com>
2022-10-15 14:43:48 +0300

    more corrections

--- services/email.js
+++ services/email.js
@@ -1,15 +1,14 @@
 import { init } from 'emailjs-com';
 
-init(process.env.NEXT_PUBLIC_USER_ID);
+init(process.env.USER_ID);
 
 function postEmail(formData) {
 
   console.log(process.env)
-  console.log(process.env.NEXT_PUBLIC_TEMPLATE_ID)
   const data = {
-    service_id: process.env.NEXT_PUBLIC_SERVICE_ID,
-    template_id: process.env.NEXT_PUBLIC_TEMPLATE_ID,
-    user_id: process.env.NEXT_PUBLIC_USER_ID,
+    service_id: process.env.SERVICE_ID,
+    template_id: process.env.TEMPLATE_ID,
+    user_id: process.env.USER_ID,
     template_params: formData && formData
   }
   return fetch('https://api.emailjs.com/api/v1.0/email/send', {
